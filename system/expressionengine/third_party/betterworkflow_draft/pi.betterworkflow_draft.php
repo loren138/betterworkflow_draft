b22c6dd675dacb6d206eebc61464d293cd90e1ac
Loren <loren@klingman.us>
2012-08-25 20:41:28 +0700

    Bug fix in preview detection

--- system/expressionengine/third_party/betterworkflow_draft/pi.betterworkflow_draft.php
+++ system/expressionengine/third_party/betterworkflow_draft/pi.betterworkflow_draft.php
@@ -2,7 +2,7 @@
 
 $plugin_info = array(
 	'pi_name' => 'Better Workflow Draft',
-	'pi_version' => '1.0',
+	'pi_version' => '1.1',
 	'pi_author' => 'Loren Klingman',
 	'pi_author_url' => 'https://github.com/wiseloren/betterworkflow_draft',
 	'pi_description' => 'Create notes that only show for better workflow drafts or only show on the live site.',
@@ -13,8 +13,9 @@ Create notes that only show for better workflow drafts or only show on the live 
 ## Parameters
 
 * draft 
-	* on  - _default_, will show only for a previewed draft
-	* off - will show only for views on the website
+	* preview  - _default_, will show only for any preview whether or not it is a draft
+	* draft - will show only for a previewed draft (drafts are unpublished versions of live entries)
+	* live - will show only for views on the website
 * parse
 	* inward - will cause the tags inside to not be parsed if the test fails
 
@@ -22,7 +23,9 @@ Create notes that only show for better workflow drafts or only show on the live 
 
 {exp:betterworkflow_draft parse="inward"}You are viewing a betterworkflow preview.{/exp:betterworkflow_draft}
 
-{exp:betterworkflow_draft parse="inward" draft="no"}You are viewing on the website.{/exp:betterworkflow_draft}',
+{exp:betterworkflow_draft parse="inward" draft="draft"}You are viewing a betterworkflow draft.{/exp:betterworkflow_draft}
+
+{exp:betterworkflow_draft parse="inward" draft="live"}You are viewing on the website.{/exp:betterworkflow_draft}',
 );
 
 /**
@@ -32,7 +35,7 @@ Create notes that only show for better workflow drafts or only show on the live 
  *
  * @author Loren Klingman
  * @link https://github.com/wiseloren/betterworkflow_draft
- * @version 1.0
+ * @version 1.1
  *
  * @property CI_Controller $EE
  */
@@ -51,19 +54,35 @@ class Betterworkflow_draft
 	public function Betterworkflow_draft()
 	{
 		$this->EE =& get_instance();
-
-		if ($this->EE->TMPL->fetch_param('draft') && $this->EE->TMPL->fetch_param('draft') == 'no') {
-			if (isset($this->EE->session->cache['ep_better_workflow']['is_draft']) 
-				&& $this->EE->session->cache['ep_better_workflow']['is_draft']) {
-				$this->EE->TMPL->log_item('Better Workflow Draft: Skipping, this is a better workflow draft.');
-				return '';
-			}
-		} else {
-			if (!isset($this->EE->session->cache['ep_better_workflow']['is_draft']) 
-				|| !$this->EE->session->cache['ep_better_workflow']['is_draft']) {
-				$this->EE->TMPL->log_item('Better Workflow Draft: Skipping, this is not a better workflow draft.');
-				return '';
-			}
+		$draft = $this->EE->TMPL->fetch_param('draft');
+		if (!$draft) {
+			$draft = "preview";
+		}
+		switch ($draft) {
+			case 'draft':
+				if (!isset($this->EE->session->cache['ep_better_workflow']['is_draft']) 
+					|| !$this->EE->session->cache['ep_better_workflow']['is_draft']) {
+					$this->EE->TMPL->log_item('Better Workflow Draft: Skipping, this is not a better workflow draft.');
+					return '';
+				}
+				break;
+			case 'live':
+				if ((isset($this->EE->session->cache['ep_better_workflow']['is_valid_preview_request']) 
+					&& $this->EE->session->cache['ep_better_workflow']['is_valid_preview_request']) || 
+					(isset($this->EE->session->cache['ep_better_workflow']['is_draft']) 
+					&& $this->EE->session->cache['ep_better_workflow']['is_draft'])) {
+					$this->EE->TMPL->log_item('Better Workflow Draft: Skipping, this is a better workflow draft or preview.');
+					return '';
+				}
+				break;
+			default:
+				if ((!isset($this->EE->session->cache['ep_better_workflow']['is_valid_preview_request']) 
+					&& !$this->EE->session->cache['ep_better_workflow']['is_valid_preview_request'])) {
+					$this->EE->TMPL->log_item('Better Workflow Draft: Skipping, this is not a better workflow preview.');
+					return '';
+				}
+				break;
+			break;
 		}
 
 		return $this->return_data = $this->EE->TMPL->tagdata;
